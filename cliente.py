d973d692f42fb22380a75c16b950227a314c6826
Marialuizadamasceno <marialuizadamceno@gmail.com>
2023-10-09 15:38:45 -0300

    cadastrar

--- cliente.py
+++ cliente.py
@@ -6,7 +6,7 @@ def cadastrar_cliente(clientes, nome , email, telefone):
         'Telefone': telefone
     }
     clientes.append(cliente)
-    print("suscesso!")
+    print("suscesso!\n")
 clientes=[] 
 def criar_arquivo_csv():
     with open('arquivo.csv', mode="w", newline="") as arquivo_csv:
@@ -15,27 +15,25 @@ def criar_arquivo_csv():
     
         for clientee in clientes: #para navegar no dicionário
              writer.writerow([clientee["Nome"], clientee["Email"], clientee["Telefone"]])
-             print("salvo!")
+             break
 
 def ler_dados_csv():
     with open('arquivo.csv', mode='r') as arquivo_csv:
         leitor_csv = csv.DictReader(arquivo_csv)
         for linha in leitor_csv:
             print(f"Nome:{linha['Nome']}, Email: {linha['Email']}, Telefone:{linha['Telefone']}")
-
-
+    
 
 while True:
     print("MENU\n"
         "1. cadastrar \n"
-        "2. imprimir\n"
-        "3. salvar\n")
+        "2. imprimir\n")
     opcao=int(input("escolha uma opção"))
 
     if opcao == 1:
-        nome = input("nome do cliente: ")
-        email = input("Email do cliente: ")
-        telefone = input("telefone do cliente: ")
+        nome = input("nome do cliente: \n")
+        email = input("Email do cliente:\n ")
+        telefone = input("telefone do cliente:\n ")
         cadastrar_cliente(clientes,nome,email,telefone)
         criar_arquivo_csv()
     elif opcao == 2:
