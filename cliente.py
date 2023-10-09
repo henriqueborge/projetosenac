38ed22a242dbd3bf06242672ff270361aee9967f
RBFERNANDESS <145044363+RBFERNANDESS@users.noreply.github.com>
2023-10-09 15:02:36 -0300

    funçao deletar

--- cliente.py
+++ cliente.py
@@ -1,4 +1,43 @@
-#função cadastrar
-#função para ler arquivo
-#função para editar arquivo
-#função deletar arquivo
+
+import csv
+
+def deletar_cliente(arquivo_csv, id_cliente):
+    nova_lista_clientes = []
+
+    
+    with open(arquivo_csv, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+
+        
+        for linha in reader:
+            
+            if linha['ID'] == id_cliente:
+                continue  
+            nova_lista_clientes.append(linha)  
+
+    
+    with open(arquivo_csv, mode='w', newline='') as file:
+        
+        campos = ['ID', 'Nome', 'Email', 'Telefone']
+
+        
+        writer = csv.DictWriter(file, fieldnames=campos)
+
+        
+        writer.writeheader()
+
+       
+        for linha in nova_lista_clientes:
+            writer.writerow(linha)
+
+    print(f"Cliente com ID {id_cliente} foi excluído com sucesso.")
+
+
+arquivo_csv = 'clientes.csv'
+id_cliente_a_deletar = '123' 
+deletar_cliente(arquivo_csv, id_cliente_a_deletar)
+
+
+
+
+
